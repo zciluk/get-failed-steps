a1ba2836d066e2be3c19b29876689f1a54a7f492
Piotr K <kulicz.pl@gmail.com>
2020-01-29 17:45:14 +0100

    Improve layout/add message box

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -19,16 +19,24 @@ function App() {
     projectID: process.env.REACT_APP_PROJECT_ID,
     isError: false,
     selectedDate: new Date().toISOString().substring(0, 10),
-    isSearching: false
+    isSearching: false,
+    pipelinesNumber: null,
+    jobsNumber: null,
+    testFailsNumber: null
   });
   const searchCall = searchTerm => {
     setState(prevState => ({
       ...prevState,
       isSearching: true,
-      isError: false
+      isError: false,
+      pipelinesNumber: null,
+      jobsNumber: null,
+      testFailsNumber: null
     }));
+    let nextDayDate = new Date(state.selectedDate);
+    nextDayDate.setDate(nextDayDate.getDate() + 1);
     return API.get(
-      `/projects/${state.projectID}/pipelines?updated_after=${state.selectedDate}T00:00:00Z&order_by=updated_at&sort=asc`,
+      `/projects/${state.projectID}/pipelines?updated_after=${state.selectedDate}T00:00:00Z&order_by=updated_at&sort=asc&updated_before=${nextDayDate}T00:00:00Z`,
       {
         headers: {
           "PRIVATE-TOKEN": state.apiKey
@@ -37,6 +45,10 @@ function App() {
     )
       .then(res => {
         retriveJobs(res.data);
+        setState(prevState => ({
+          ...prevState,
+          pipelinesNumber: res.data.length
+        }));
       })
       .catch(error => {
         setState(prevState => ({
@@ -80,9 +92,13 @@ function App() {
           })
       );
     });
-    Promise.all(callsCollection).then(() =>
-      retrieveAndProcessArtifacts(jobsCollection)
-    );
+    Promise.all(callsCollection).then(() => {
+      retrieveAndProcessArtifacts(jobsCollection);
+      setState(prevState => ({
+        ...prevState,
+        jobsNumber: jobsCollection.length
+      }));
+    });
   };
   const retrieveAndProcessArtifacts = receivedJobs => {
     let stepsContainer = [];
@@ -112,7 +128,8 @@ function App() {
         ...prevState,
         isError: false,
         isSearching: false,
-        displayedData: stepsContainer
+        displayedData: stepsContainer,
+        testFailsNumber: stepsContainer.length
       }));
     });
   };
@@ -120,14 +137,16 @@ function App() {
     <Segment>
       <Segment inverted color="teal">
         <Grid divided="vertically">
-          <Grid.Row columns={4}>
+          <Grid.Row columns={3}>
             <GridColumn>
               <Input
-                icon="folder open"
+                icon="gitlab"
                 name="projectID"
                 value={state.projectID}
                 onChange={handleChange}
                 placeholder="Project ID"
+                label={{ basic: true, content: "Project ID" }}
+                labelPosition="left"
               />
             </GridColumn>
             <GridColumn>
@@ -137,10 +156,10 @@ function App() {
                 value={state.apiKey}
                 onChange={handleChange}
                 placeholder="API key"
+                label={{ basic: true, content: "API key" }}
+                labelPosition="left"
               />
             </GridColumn>
-          </Grid.Row>
-          <Grid.Row columns={4}>
             <GridColumn>
               <Input
                 icon="clock"
@@ -148,9 +167,15 @@ function App() {
                 value={state.selectedDate}
                 onChange={handleChange}
                 placeholder="Selected Day"
+                label={{ basic: true, content: "Day to check" }}
+                labelPosition="left"
+                type="date"
               />
             </GridColumn>
+          </Grid.Row>
+          <Grid.Row columns="2">
             <GridColumn>
+              {}
               <Button onClick={searchCall} color="black">
                 Get results
               </Button>
@@ -159,37 +184,61 @@ function App() {
           </Grid.Row>
         </Grid>
       </Segment>
-      {!state.isError && state.displayedData.length !== 0 && (
-        <Table collapsing color="teal">
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Name of test</Table.HeaderCell>
-              <Table.HeaderCell>Number of Fails</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
+      <Grid columns="equal">
+        <GridColumn width="8">
+          {!state.isError &&
+            !state.isSearching &&
+            state.displayedData.length !== 0 && (
+              <Table collapsing color="teal">
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Name of test</Table.HeaderCell>
+                    <Table.HeaderCell>Number of Fails</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
 
-          <Table.Body>
-            {state.displayedData
-              .filter((item, i, ar) => ar.indexOf(item) === i)
-              .map(rowData => (
-                <Table.Row>
-                  <Table.Cell>{rowData}</Table.Cell>
-                  <Table.Cell>
-                    {state.displayedData.filter(x => x === rowData).length}
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-          </Table.Body>
-        </Table>
-      )}
-      {state.isError && (
-        <Message negative>
-          <Message.Header>
-            There was an error while doing API request
-          </Message.Header>
-        </Message>
-      )}
-      {state.isSearching && <Spinner />}
+                <Table.Body>
+                  {state.displayedData
+                    .filter((item, i, ar) => ar.indexOf(item) === i)
+                    .map(rowData => (
+                      <Table.Row>
+                        <Table.Cell>{rowData}</Table.Cell>
+                        <Table.Cell>
+                          {
+                            state.displayedData.filter(x => x === rowData)
+                              .length
+                          }
+                        </Table.Cell>
+                      </Table.Row>
+                    ))}
+                </Table.Body>
+              </Table>
+            )}
+          {state.isError && (
+            <Message negative>
+              <Message.Header>
+                There was an error while doing API request. Check if API
+                key/Project ID is correct.
+              </Message.Header>
+            </Message>
+          )}
+          {state.isSearching && <Spinner />}
+        </GridColumn>
+        <GridColumn width="4">
+          {!state.isError && state.pipelinesNumber !== null && (
+            <Message>
+              <p>Check for date: {state.selectedDate} </p>
+              <p>Pipelines retrieved: {state.pipelinesNumber} </p>
+              {state.jobsNumber !== null && (
+                <p> Jobs retrieved: {state.jobsNumber} </p>
+              )}
+              {state.testFailsNumber !== null && (
+                <p> Failed steps: {state.testFailsNumber} </p>
+              )}
+            </Message>
+          )}
+        </GridColumn>
+      </Grid>
     </Segment>
   );
 }
