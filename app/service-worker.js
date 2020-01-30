5a741bbb3b419c1e332d8a9ff72c854b16e3e64f
Piotr K <kulicz.pl@gmail.com>
2020-01-30 10:28:05 +0100

    Upload /app to public

--- app/service-worker.js
+++ app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "zciluk.github.io/get-failed-steps/app/precache-manifest.693f153c298e9775e48c6ba52a752480.js"
+  "/get-failed-steps/app/precache-manifest.590681ba66272d34698f6a01c5a1e09c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("zciluk.github.io/get-failed-steps/app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/get-failed-steps/app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
